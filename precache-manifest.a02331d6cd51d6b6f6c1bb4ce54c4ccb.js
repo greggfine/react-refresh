041f6dbfafda90a223df3f7e6689a81278fa7dc9
Gregg Fine <bbeez8500@gmail.com>
2020-10-09 16:36:36 -0400

    Updates

---
+++ precache-manifest.a02331d6cd51d6b6f6c1bb4ce54c4ccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c4c90d255d30d0159b22540176c073",
+    "url": "/react-refresh/index.html"
+  },
+  {
+    "revision": "1e5192582a19b714d15e",
+    "url": "/react-refresh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1eac1c77cc431639cad5",
+    "url": "/react-refresh/static/js/2.badbfd28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-refresh/static/js/2.badbfd28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5192582a19b714d15e",
+    "url": "/react-refresh/static/js/main.bbcf0bdf.chunk.js"
+  },
+  {
+    "revision": "2175e077aeaec159790f",
+    "url": "/react-refresh/static/js/runtime-main.6e065cd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-refresh/static/media/logo.5d5d9eef.svg"
+  }
+]);
