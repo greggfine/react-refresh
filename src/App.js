b68f1dab9f3c686084874ce53d94c8d00a14920c
Gregg Fine <bbeez8500@gmail.com>
2020-10-09 16:41:09 -0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from "./Form"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
-        <h1>Test App</h1>
-        <Form />
+      <h1>Test App V.2</h1>
+      <Form />
     </div>
   );
 }
